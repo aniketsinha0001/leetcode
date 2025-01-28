51d457bfa434d5b8565ba038ad5daaa1382ed6d0
Aniket Sinha <37899215+aniketsinha0001@users.noreply.github.com>
2025-01-28 23:57:10 +0530

    MergeSortedArray

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -9,28 +9,28 @@ public static void main(String[] args) {
 		int n = 3;
 		new MergeSortedArray().merge(nums1, m, nums2, n);
 	}
+	
+public void merge(int[] nums1, int m, int[] nums2, int n) {
+		  int[] temp = new int[m + n];
+		    int i = 0, j = 0, k = 0;
+		    for (; i < m && j < n; k++) {
+		        if (nums1[i] <= nums2[j]) {
+		            temp[k] = nums1[i++];
+		        } else {
+		            temp[k] = nums2[j++];
+		        }
+		    }
 
-	public void merge(int[] nums1, int m, int[] nums2, int n) {
-		int k = 0;
-		int t = 0;
-		int temp[] = new int[m + n];
-		for (int i = 0, j = 0; i < m && j < n;) {
-			t = j;
-			if (nums1[i] < nums2[j] && i < m) {
-				temp[k++] = nums1[i++];
-			} else if (nums1[i] == nums2[j] && j < n) {
-				temp[k++] = nums1[i++];
-				temp[k++] = nums2[j++];
-			} else {
-				temp[k++] = nums2[j++];
-			}
-		}
-		for (int i = t; i < n; i++) {
-			temp[k++] = nums2[i];
-		}
-		nums1 = temp.clone();
-		for (int i = 0; i < nums1.length; i++) {
-			System.out.print(nums1[i]+" ");
-		}
+		    for (; i < m; i++, k++) {
+		        temp[k] = nums1[i];
+		    }
+
+		    for (; j < n; j++, k++) {
+		        temp[k] = nums2[j];
+		    }
+		    for (i = 0; i < nums1.length; i++) {
+		        nums1[i] = temp[i];
+		        System.out.println(nums1[i]);
+		    }
 	}
 }
