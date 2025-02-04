272cb797dbc7e667121cb58d128cf29f20b17613
Aniket Sinha <37899215+aniketsinha0001@users.noreply.github.com>
2025-02-04 22:32:11 +0530

    Remove Element

---
+++ RemoveElement.java
@@ -0,0 +1,22 @@
+package com.sinha.aniket.array;
+
+public class RemoveElement {
+
+	public static void main(String[] args) {
+		int nums[] = {3,2,2,3};
+		int val = 3;
+		int result = new RemoveElement().removeElement(nums,val);
+		System.out.println(result);
+	}
+
+	public int removeElement(int[] nums, int val) {
+		int k = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == val) {
+				continue;
+			}
+			nums[k++] = nums[i];
+		}
+		return k;
+	}
+}
