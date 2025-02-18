4a19c997fb91fdf835096b4fed658336408f7d99
Aniket Sinha <37899215+aniketsinha0001@users.noreply.github.com>
2025-02-18 20:16:26 +0530

    LongestCommonPrefix

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,40 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+
+	public static void main(String[] args) {
+		String strs[] = {"flower","flow","flight"};
+		new LongestCommonPrefix().longestCommonPrefix(strs);
+		
+	}
+    public String longestCommonPrefix(String[] strs) {
+	    	Arrays.sort(strs);
+			String first = strs[0];
+			String last = strs[strs.length - 1];
+			String common = checkFirstLast(first, last);
+			System.out.println(common);
+			return common;
+		}
+
+		private String checkFirstLast(String first, String last) {
+			int n = first.length();
+			int m = last.length();
+			int small = 0;
+			String common = "";
+			if (m > n) {
+				small = n;
+			} else {
+				small = m;
+			}
+			for (int i = 0; i < small; i++) {
+				if (first.charAt(i) == last.charAt(i)) {
+					common = common + first.charAt(i);
+				} else {
+					break;
+				}
+			}
+			return common;
+	    }
+}
