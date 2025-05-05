815f6b8f7e5df69f431fe9f083dc7159bf8b6c8e
Aniket Sinha <37899215+aniketsinha0001@users.noreply.github.com>
2025-05-05 22:50:30 +0530

    Sqrt

---
+++ Sqrt.java
@@ -0,0 +1,17 @@
+package com.leetcode.solution;
+
+public class Sqrt {
+
+	public static void main(String[] args) {
+		int x = 8;
+		int result = new Sqrt().mySqrt(x);
+		System.out.println(result);
+	}
+
+	public int mySqrt(int x) {
+
+		return (int) Math.sqrt(x);
+
+	}
+
+}
