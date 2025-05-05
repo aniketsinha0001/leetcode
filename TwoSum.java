4817395d14cbb0928e1add077d175211ba1411f0
Aniket Sinha <37899215+aniketsinha0001@users.noreply.github.com>
2025-05-05 23:20:04 +0530

    TwoSum

---
+++ TwoSum.java
@@ -0,0 +1,27 @@
+package com.leetcode;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		int nums[] = { 2,7,11,15 };
+		int k = 9;
+		int[] sum = twoSum(nums, k);
+		for(int i=0;i<sum.length;i++)
+		System.out.println(sum[i]);
+	}
+
+	public static int[] twoSum(int[] nums, int target) {
+		int[] result = new int[2];
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					result[0] = i;
+					result[1] = j;
+					return result;
+				}
+			}
+		}
+		return result;
+	}
+
+}
