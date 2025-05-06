e611300ea0326a78959bbead8421a7ee0eacf09c
Aniket Sinha <37899215+aniketsinha0001@users.noreply.github.com>
2025-05-06 16:14:03 +0530

    Update IsSubsequence.java

--- IsSubsequence.java
+++ IsSubsequence.java
@@ -8,6 +8,21 @@ public class IsSubsequence {
 	}
 
 	public boolean isSubsequence(String s, String t) {
+		int sLength = s.length();
+		int tLength = t.length();
+		int j = 0; 
+		for (int i = 0; i < tLength && j < sLength; i++) {
+			if (t.charAt(i) == s.charAt(j)) {
+				j++;
+			}
+		}
+		if (sLength == j) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isSubsequenceOld(String s, String t) {
 		int m = s.length(), n = t.length();
 		int i = 0, j = 0;
 		for (; i < n && j < m;) {
