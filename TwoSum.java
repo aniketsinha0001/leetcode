6bf475fd945560f9b5d647a1f8c0e1d2ff5e4fcd
Aniket Sinha <37899215+aniketsinha0001@users.noreply.github.com>
2025-05-06 12:40:29 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -9,8 +9,20 @@ public class TwoSum {
 		for(int i=0;i<sum.length;i++)
 		System.out.println(sum[i]);
 	}
-
+	
 	public static int[] twoSum(int[] nums, int target) {
+	Map<Integer, Integer> hm = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			int diff = target - nums[i];
+			if (hm.containsKey(diff)) {
+				return new int[] { hm.get(diff), i };
+			}
+			hm.put(nums[i], i);
+		}
+	return new int[] {};
+	}
+
+	public static int[] twoSumOld(int[] nums, int target) {
 		int[] result = new int[2];
 		for (int i = 0; i < nums.length; i++) {
 			for (int j = i + 1; j < nums.length; j++) {
